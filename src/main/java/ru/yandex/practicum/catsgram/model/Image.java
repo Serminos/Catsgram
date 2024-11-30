a6749e500c528f9b249fa2ca741f0f2a06d15605
KuznetsovOS <KuznetsovOS@kadastr.ru>
2024-12-01 00:56:22 +0300

    Catsgram first version

---
+++ src/main/java/ru/yandex/practicum/catsgram/model/Image.java
@@ -0,0 +1,11 @@
+package ru.yandex.practicum.catsgram.model;
+
+import lombok.Data;
+
+@Data
+public class Image {
+    Long id; //  уникальный идентификатор изображения,
+    long postId; //  уникальный идентификатор поста, к которому прикреплено изображение,
+    String originalFileName; //  имя файла, который содержит изображение,
+    String filePath; //  путь, по которому изображение было сохранено
+}
